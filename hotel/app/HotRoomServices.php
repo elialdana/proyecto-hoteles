bbd1cbcecdf811941f7c2c6edae2733401aca59f
elialdana <valdanap@miumg.edu.gt>
2018-10-04 02:46:47 -0300

    mte servicios

---
+++ hotel/app/HotRoomServices.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HotRoomServices extends Model
+{
+
+
+    protected $fillable = ['name','description','price'];
+}
---
+++ hotel/app/Http/Controllers/RoomServicesController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\HotRoomType;
+
+class RoomServicesController extends Controller
+{
+    public function getAll(){
+
+        $data = HotRoomType::all();
+
+        return view('roomServicesVw',compact('data'));
+    }
+
+    public function create(){
+        return view('roomServicesCreateVw');
+    }
+
+    public function store(){
+
+        $data = request()->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'description' => ''
+        ], [
+            'name.required' => 'El campo nombre es requerido',
+            'price.required' => 'El campo precio es requerido'
+        ]);
+
+        //dd($data);
+
+        HotRoomType::create([
+            'name' => $data['name'],
+            'price' => $data['price'],
+            'description' => $data['description']
+        ]);
+
+        return redirect()->route('roomServicesNew');
+    }
+
+    public function edit(HotRoomServices $hotRoomServices){
+        return view ('roomServicesEditVw', ['hotRoomServices' => $hotRoomServices]);
+    }
+
+    public function update(HotRoomServices $hotRoomServices){
+
+        $data = request()->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'description' => ''
+        ]);
+
+        $hotRoomType->update($data);
+
+        return redirect()->route('roomServices', ['hotRoomServices', $hotRoomServices]);
+    }
+
+    public function destroy(HotRoomServices $hotRoomServices){
+
+        $hotRoomServices->delete();
+
+        return redirect()->route('roomServices');
+    }
+
+}
